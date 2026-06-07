571d95717b817b0f6d466899a7a6f265b0079e0d
LIZZYGREAT <2660550447@qq.com>
2026-06-07 13:43:23 +0800

    更新注释等内容

--- database/scripts/trigger_insert.sql
+++ database/scripts/trigger_insert.sql
@@ -1,1 +1,17 @@
-DROP TRIGGER IF EXISTS AfterUserRegister;
+--DROP TRIGGER IF EXISTS AfterUserRegister;
+
+DELIMITER //
+CREATE TRIGGER AfterUserRegister
+AFTER INSERT ON User
+FOR EACH ROW
+BEGIN
+    INSERT INTO UserProfile (user_id, role) 
+    VALUES (NEW.user_id, '访客');
+    
+    INSERT INTO UserPreference (user_id, category, weight) 
+    VALUES (NEW.user_id, '新闻', 1.0);
+    
+    INSERT INTO UserPreference (user_id, category, weight) 
+    VALUES (NEW.user_id, '综合', 1.0);
+END //
+DELIMITER ;
